01a55cd68ff2ac31fa72567a9a9d5d67319ee5b9
hasan-live <111642873+hasan-live@users.noreply.github.com>
2022-10-06 08:16:41 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+
+import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
+import './App.css';
+import { BeakerIcon } from '@heroicons/react/solid'
+import About from './components/Pages/About/About';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Pages/Home/Home';
+import Movies from './components/Pages/Movies/Movies';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import axios from 'axios';
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import Blogs from './components/Pages/Blogs';
+import Movie from './components/Pages/Movies/Movie';
+import Lives from './components/Pages/Lives/Lives';
+import TvShows from './components/Pages/TvShows/TvShows';
+
+
+
+function App() {
+  return (
+    <div className="App overflow-hidden">
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movie" element={<Movie />} />
+        <Route path="lives" element={<Lives />} />
+        <Route path="tvshows" element={<TvShows />} />
+        <Route path="blogs" element={<Blogs />} />
+      </Routes>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
