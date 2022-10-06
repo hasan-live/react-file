7640116cc229021f5fd4a4aa5fa79846b5497017
hasan-live <111642873+hasan-live@users.noreply.github.com>
2022-10-06 08:27:44 +0600

    Add files via upload

---
+++ components/Pages/TvShows/TvShows.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TvShowSliders from './TvShowSliders';
+import TvShowTranding from './TvShowTranding';
+
+const TvShows = () => {
+    return (
+        <>
+          <TvShowSliders></TvShowSliders>  
+          <TvShowTranding></TvShowTranding>
+        </>
+    );
+};
+
+export default TvShows;
